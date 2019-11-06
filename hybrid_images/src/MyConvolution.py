808abc7f34cf8f98d977a0a7d152ca5c858f01ef
thanakorn <thanakorn.panyapiang@outlook.com>
2019-11-06 19:58:22 +0000

    Add flipping template

--- hybrid_images/src/MyConvolution.py
+++ hybrid_images/src/MyConvolution.py
@@ -13,20 +13,22 @@ def convolve(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
 	:returns the convolved image (of the same shape as the input image)
 	:rtype numpy.ndarray
 	"""
+	kheight,kwidth = kernel.shape
+	padded_image = zero_pad(image, int(kheight/2), int(kwidth/2))
 	if (image.ndim > 2):
-		height, width, channel = image.shape
+		height, width, channel = padded_image.shape
 		result_image = np.zeros((height, width, channel))
 		for c in range(channel):
-			result_image[:,:,c] = convolve_img(image[:,:,c], kernel)
-		return result_image
+			result_image[:,:,c] = convolve_img(padded_image[:,:,c], kernel)
+		return remove_pad(result_image, int(kheight/2), int(kwidth/2))
 	else:
-		return convolve_img(image, kernel)
+		return remove_pad(convolve_img(padded_image, kernel), int(kheight/2), int(kwidth/2))
 
 def convolve_img(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+	flipped_kernel = np.flip(kernel)
 	height, width = image.shape
 	kheight,kwidth = kernel.shape
 	result_image = np.zeros((height, width))
-
 	height_start = int(kheight / 2)
 	height_end = height - int(kheight / 2)
 	width_start = int(kwidth / 2)
@@ -40,7 +42,38 @@ def convolve_img(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
 				col_start = j - int(kwidth / 2)
 				col_end = j + int(kwidth / 2) + 1
 				region = image[row_start:row_end, col_start:col_end]
-				result_image[i,j] = np.sum(region * kernel)
+				result_image[i,j] = np.sum(region * flipped_kernel)
 			else:
 				result_image[i,j] = image[i, j]
 	return result_image
+
+def zero_pad(image: np.ndarray, num_row_pad, num_col_pad) -> np.ndarray:
+	if(image.ndim > 2):
+		height, width, channel = image.shape
+		padded_img = np.zeros((height + (num_row_pad * 2), width  + (num_col_pad * 2), channel))
+		for c in range(channel):
+			padded_img[:,:,c] = pad(0, image[:,:,c], num_row_pad, num_col_pad)
+		return padded_img
+	else:
+		return pad(0, image, num_row_pad, num_col_pad)
+
+def remove_pad(image: np.ndarray, num_row_pad, num_col_pad) -> np.ndarray:
+	if(image.ndim > 2):
+		height, width, channel = image.shape
+		unpadded_img = np.zeros((height - (num_row_pad * 2), width  - (num_col_pad * 2), channel))
+		for c in range(channel):
+			unpadded_img[:,:,c] = unpad(image[:,:,c], num_row_pad, num_col_pad)
+		return unpadded_img
+	else:
+		return unpad(image, num_row_pad, num_col_pad)
+
+def pad(pad_value, image: np.ndarray, num_row_pad, num_col_pad):
+    height, width = image.shape
+    padded_img = np.zeros((height + (num_row_pad * 2), width + (num_col_pad * 2)))
+    padded_img[num_row_pad: height + num_row_pad, num_col_pad: width + num_col_pad] = image
+    return padded_img
+
+def unpad(image: np.ndarray, num_row_pad, num_col_pad):
+	height, width = image.shape
+	unpadded_img = image[num_row_pad: height - num_row_pad, num_col_pad: width - num_col_pad]
+	return unpadded_img
--- hybrid_images/test/MyConvolutionTests.py
+++ hybrid_images/test/MyConvolutionTests.py
@@ -1,10 +1,12 @@
 import unittest
 import numpy as np
+import scipy
+from scipy import ndimage
 import sys
 
 sys.path.append('/home/tpanyapiang/git/MSc/computer_vision/hybrid_images/src')
 
-from MyConvolution import convolve
+from MyConvolution import convolve, zero_pad, remove_pad
 from numpy.testing import assert_array_equal
 
 class TestMyConvolution(unittest.TestCase):
@@ -25,14 +27,7 @@ class TestMyConvolution(unittest.TestCase):
                 [0, 0, 0],
                 [1, 2, 1]
             ]))
-        expected = np.ndarray(
-            shape = (3,3), 
-            dtype = int,
-            buffer = np.array([
-                [48, 41, 43],
-                [42, 7, 44],
-                [45, 46, 43]
-            ]))
+        expected = ndimage.convolve(img, kernel, mode='constant', cval=0)
         result = convolve(img, kernel)
         assert_array_equal(result, expected)
 
@@ -55,80 +50,7 @@ class TestMyConvolution(unittest.TestCase):
                 [2, 2, 0],
                 [0, 1, 2]
             ]))
-        expected = np.ndarray(
-            shape = (5,5), 
-            dtype = int,
-            buffer = np.array([
-                [3, 3, 2, 1, 0],
-                [0, 12, 12, 17, 1],
-                [3, 10, 17, 19, 3],
-                [2, 9, 6, 14, 2],
-                [2, 0, 0, 0, 1],
-            ]))
-        result = convolve(img, kernel)
-        assert_array_equal(result, expected)
-
-    def test_zero_padding_img(self):
-        img = np.ndarray(
-            shape = (5,5), 
-            dtype = int,
-            buffer = np.array([
-                [0,  0,  0,  0, 0],
-                [0, 48, 41, 43, 0],
-                [0, 42, 47, 44, 0],
-                [0, 45, 46, 43, 0],
-                [0,  0,  0,  0, 0]
-            ]))
-        kernel = np.ndarray(
-            shape = (3,3),
-            dtype = int,
-            buffer = np.array([
-                [-1, -2, -1],
-                [0, 0, 0],
-                [1, 2, 1]
-            ]))
-        expected = np.ndarray(
-            shape = (5,5), 
-            dtype = int,
-            buffer = np.array([
-                [0,  0,  0,  0, 0],
-                [0, 131, 180, 135, 0],
-                [0, -1, 7, 5, 0],
-                [0, -131, -180, -135, 0],
-                [0,  0,  0,  0, 0]
-            ]))
-        result = convolve(img, kernel)
-        assert_array_equal(result, expected)
-    
-    def test_img_edge_not_change(self):
-        img = np.ndarray(
-            shape = (5,5), 
-            dtype = int,
-            buffer = np.array([
-                [3, 3, 2, 1, 0],
-                [0, 0, 1, 3, 1],
-                [3, 1, 2, 2, 3],
-                [2, 0, 0, 2, 2],
-                [2, 0, 0, 0, 1],
-            ]))
-        kernel = np.ndarray(
-            shape = (3,3),
-            dtype = int,
-            buffer = np.array([
-                [0, 1, 2],
-                [2, 2, 0],
-                [0, 1, 2]
-            ]))
-        expected = np.ndarray(
-            shape = (5,5), 
-            dtype = int,
-            buffer = np.array([
-                [3, 3, 2, 1, 0],
-                [0, 12, 12, 17, 1],
-                [3, 10, 17, 19, 3],
-                [2, 9, 6, 14, 2],
-                [2, 0, 0, 0, 1],
-            ]))
+        expected = ndimage.convolve(img, kernel, mode='constant', cval=0)
         result = convolve(img, kernel)
         assert_array_equal(result, expected)
 
@@ -151,16 +73,7 @@ class TestMyConvolution(unittest.TestCase):
                 [2, 2, 0],
                 [0, 1, 2]
             ]))
-        expected = np.ndarray(
-            shape = (5,4), 
-            dtype = int,
-            buffer = np.array([
-                [3, 3, 2, 1],
-                [0, 12, 12, 3],
-                [3, 10, 17, 2],
-                [2, 9, 6, 2],
-                [2, 0, 0, 0],
-            ]))
+        expected = ndimage.convolve(img, kernel, mode='constant', cval=0)
         result = convolve(img, kernel)
         assert_array_equal(result, expected)
 
@@ -183,16 +96,7 @@ class TestMyConvolution(unittest.TestCase):
                 [2, 2, 0, 1, 1],
                 [0, 1, 2, 1, 1]
             ]))
-        expected = np.ndarray(
-            shape = (5,5), 
-            dtype = int,
-            buffer = np.array([
-                [3, 3, 2, 1, 0],
-                [0, 0, 23, 3, 1],
-                [3, 1, 26, 2, 3],
-                [2, 0, 21, 2, 2],
-                [2, 0, 0, 0, 1],
-            ]))
+        expected = ndimage.convolve(img, kernel, mode='constant', cval=0)
         result = convolve(img, kernel)
         assert_array_equal(result, expected)
     
@@ -215,16 +119,7 @@ class TestMyConvolution(unittest.TestCase):
                 [2, 2, 0, 1, 1],
                 [0, 1, 2, 1, 1]
             ]))
-        expected = np.ndarray(
-            shape = (5,6), 
-            dtype = int,
-            buffer = np.array([
-                [3, 3, 2, 1, 0, 1],
-                [0, 0, 23, 20, 1, 1],
-                [3, 1, 26, 29, 3, 2],
-                [2, 0, 21, 21, 2, 2],
-                [2, 0, 0, 0, 1, 2],
-            ]))
+        expected = ndimage.convolve(img, kernel, mode='constant', cval=0)
         result = convolve(img, kernel)
         assert_array_equal(result, expected)
 
@@ -251,22 +146,61 @@ class TestMyConvolution(unittest.TestCase):
                 [0, 0, 0],
                 [1, 2, 1]
             ]))
-        expected = np.ndarray(
-            shape = (3,3,3), 
+        expected = ndimage.convolve(np.ndarray(
+            shape = (3,3), 
             dtype = int,
-            buffer = np.array([[
-                [[48, 48, 48],
-                [41, 41, 41],
-                [43, 43, 43]],
-                [[42, 42, 42],
-                [7, 7, 7],
-                [44, 44, 44]],
-                [[45, 45, 45],
-                [46, 46, 46],
-                [43, 43, 43]]
-            ]]))
+            buffer = np.array([
+                [48, 41, 43],
+                [42, 47, 44],
+                [45, 46, 43]
+            ])), kernel, mode='constant', cval=0)
         result = convolve(img, kernel)
-        assert_array_equal(result, expected)
+        np.testing.assert_array_equal(result[:,:,0], expected)
+        np.testing.assert_array_equal(result[:,:,1], expected)
+        np.testing.assert_array_equal(result[:,:,2], expected)
+
+
+    def test_pad_img(sefl):
+        img = np.array([[1,1,1], [1,1,1], [1,1,1]])
+        expected_img = np.array([[0,0,0,0,0], [0,1,1,1,0], [0,1,1,1,0], [0,1,1,1,0], [0,0,0,0,0]])
+        np.testing.assert_array_equal(zero_pad(img, 1, 1), expected_img)
+
+    def test_pad_img_asym(self):
+        img = np.array([[1,1,1], [1,1,1], [1,1,1]])
+        num_row_pad = 1
+        num_col_pad = 2
+        expected_img = np.array([[0,0,0,0,0,0,0], [0,0,1,1,1,0,0], [0,0,1,1,1,0,0], [0,0,1,1,1,0,0], [0,0,0,0,0,0,0]])
+        np.testing.assert_array_equal(zero_pad(img, num_row_pad, num_col_pad), expected_img)
+
+    def test_pad_3d_img(self):
+        img = np.ndarray(shape = (3,3,3))
+        img.fill(1)
+        expected_img = np.array([[0,0,0,0,0], [0,1,1,1,0], [0,1,1,1,0], [0,1,1,1,0], [0,0,0,0,0]])
+        padded_img = zero_pad(img, 1, 1)
+        np.testing.assert_array_equal(padded_img[:,:,0], expected_img)
+        np.testing.assert_array_equal(padded_img[:,:,1], expected_img)
+        np.testing.assert_array_equal(padded_img[:,:,2], expected_img)
+
+    def test_remove_pad(self):
+        img = np.array([[0,0,0,0,0], [0,1,1,1,0], [0,1,1,1,0], [0,1,1,1,0], [0,0,0,0,0]])
+        expected_img = np.array([[1,1,1], [1,1,1], [1,1,1]])
+        np.testing.assert_array_equal(remove_pad(img, 1, 1), expected_img)
+
+    def test_remove_pad_asym(self):
+        img = np.array([[0,0,0,0,0,0,0], [0,0,1,1,1,0,0], [0,0,1,1,1,0,0], [0,0,1,1,1,0,0], [0,0,0,0,0,0,0]])
+        num_row_pad = 1
+        num_col_pad = 2
+        expected_img =  np.array([[1,1,1], [1,1,1], [1,1,1]])
+        np.testing.assert_array_equal(remove_pad(img, num_row_pad, num_col_pad), expected_img)
+
+    def test_remove_pad_3d_img(self):
+        img = np.ndarray(shape = (5,5,3))
+        img.fill(1)
+        expected_img = np.array([[1,1,1], [1,1,1], [1,1,1]])
+        unpadded_img = remove_pad(img, 1, 1)
+        np.testing.assert_array_equal(unpadded_img[:,:,0], expected_img)
+        np.testing.assert_array_equal(unpadded_img[:,:,1], expected_img)
+        np.testing.assert_array_equal(unpadded_img[:,:,2], expected_img)
 
 if __name__ == '__main__':
     unittest.main()
